4abd4da47b8844b7dbf7246b27e500d1c24094d1
Akash walzade <walzadeakash2000@gmail.com>
2024-12-11 22:47:40 +0530

    debugger code

--- Selenium_python/Debugger.py
+++ Selenium_python/Debugger.py
@@ -27,7 +27,7 @@
 # else:
 #     print("k is not present")
 
-
+print(234)
 def check_even_or_odd(number_take):
     if number_take % 2 == 0:
         print(f'{number_take} is even number')
@@ -35,4 +35,22 @@ def check_even_or_odd(number_take):
     else:
         print(f'{number_take} is odd number')
 
+
+
+def multiply_two_numbers(number2 = 3, numbers1 = 5):
+    print(f'numbers2 is {number2}')
+    print(f'number1 is {numbers1}')
+
+    numtiply_result = number2 * numbers1
+
+    print(numtiply_result)
+    return numtiply_result
+
+
+multiply_two_numbers()
+
+
+
 check_even_or_odd(9)
+
+
